a992f0658b71b97a1fb009e4a0cf5962b792f8fa
Vardan <varnar@users.noreply.github.com>
2018-05-16 13:40:16 -0700

    Update screen.sh

--- screen.sh
+++ screen.sh
@@ -1,5 +1,5 @@
-If you error for screen "Cannot open your terminal '/dev/pts/7' - please check."
-Put in your .bashrc following function:
+# If you error for screen "Cannot open your terminal '/dev/pts/7' - please check."
+# Put in your .bashrc following function:
 
 function screen() {
   /usr/bin/script -q -c "/usr/bin/screen ${*}" /dev/null
